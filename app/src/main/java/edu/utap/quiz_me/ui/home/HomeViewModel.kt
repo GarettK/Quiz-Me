7fd4231aaafd221735711c0f3665535b705bd2e4
Garett Kinney <garettkinney@gmail.com>
2020-12-15 18:00:40 +0900

    Initial commit

---
+++ app/src/main/java/edu/utap/quiz_me/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package edu.utap.quiz_me.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
