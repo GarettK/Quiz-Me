5c629de01f7323a50cdddc442e8d2ebc0bdfc0a7
Garett Kinney <garettkinney@gmail.com>
2020-12-30 19:29:06 -0500

    Add game over screen

--- app/src/main/java/edu/utap/quiz_me/UI/GameOver.kt
+++ app/src/main/java/edu/utap/quiz_me/UI/GameOver.kt
@@ -16,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Game_Screen.newInstance] factory method to
+ * Use the [GameOver.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Game_Screen : Fragment() {
+class GameOver : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,12 +32,10 @@ class Game_Screen : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_screen, container, false)
+        return inflater.inflate(R.layout.fragment_game_over, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -46,10 +44,17 @@ class Game_Screen : Fragment() {
         val navHostFragment =
                 requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val trueButton = requireActivity().findViewById<Button>(R.id.True_Button)
-        trueButton.setOnClickListener {
-            navController.navigate(R.id.action_game_Screen_to_gameOver)
+
+        val playAgain = requireActivity().findViewById<Button>(R.id.Play_Again_Button)
+        playAgain.setOnClickListener {
+            navController.navigate(R.id.action_gameOver_to_game_Screen)
         }
+
+        val homeButton = requireActivity().findViewById<Button>(R.id.Home_Button)
+        homeButton.setOnClickListener {
+            navController.navigate(R.id.action_gameOver_to_navigation_home)
+        }
+
     }
 
     companion object {
@@ -59,16 +64,16 @@ class Game_Screen : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Game_Screen.
+         * @return A new instance of fragment GameOver.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Game_Screen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                GameOver().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
